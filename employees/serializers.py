fb5a3f95e7dd59a103e7e34b4aa5704fe5f62f27
KotadiyaAnkit <ankitkotadiya993@gmail,com>
2025-07-10 15:09:52 +0530

    project upolad

---
+++ employees/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from employees import constants as employees_constants
+
+class EmployesSerializers(serializers.Serializer):
+    # emp_id = serializers.UUIDField(db_index=True)
+    # emp_name = serializers.CharField(max_length=30)
+    # emp_designation = serializers.CharField(choices=employees_constants.STREAM_CHOICES)
+    # emp_datajoing = serializers.DateField()
+    # emp_project  = serializers.CharField(max_length=49)
+    # emp_id = serializers.UUIDField()
+    
+    emp_name = serializers.CharField(max_length=30)
+    emp_designation = serializers.ChoiceField(choices=employees_constants.STREAM_CHOICES)
+    emp_datajoing = serializers.DateField()
+    emp_project = serializers.CharField(max_length=49)
