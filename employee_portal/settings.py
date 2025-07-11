05bfc5687d9b3ebe01ec1de7def0243159eb5084
KotadiyaAnkit <ankitkotadiya993@gmail,com>
2025-07-11 15:06:38 +0530

    project

--- employee_portal/settings.py
+++ employee_portal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'employee_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employees',
+        'USER': 'postgres',
+        'PASSWORD':'12345',
+        'HOST':'localhost',
+        'POST': 5432
     }
 }
 
